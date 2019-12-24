a0272f20f06d6da79b26cad381337c40fd80187f
gokul <gokul.kannambally_balachandran@student.po.edu.pl>
2019-12-24 16:45:18 +0100

    understood the difference between container and padding widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.deepPurpleAccent,
       ),
-      body:Container(
-        color: Colors.grey[400],
+      body:Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text('Hello World'),
       ),
 
       floatingActionButton: FloatingActionButton(
