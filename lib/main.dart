9615b94f284bfdafc7b9d22f238460045991976e
gokul <gokul.kannambally_balachandran@student.po.edu.pl>
2019-12-24 16:10:51 +0100

    chenged the text for the button

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Home extends StatelessWidget {
       body: Center(
         child: IconButton(
           onPressed: (){
-            print('you clicked me');
+            print('this branch is containers and padding');
           },
           icon: Icon(Icons.alternate_email),
           color: Colors.blue,
